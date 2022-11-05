cda9b458f8ac43d22818ed9e5797c5c720e95a32
Luis Melendez <melendez12341@gmail.com>
2022-11-04 20:51:09 -0400

    feat: added image controller

---
+++ src/files/helpers/index.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import { v4 as uuid } from 'uuid';
+export const fileFilter = (
+  req: Express.Request,
+  file: Express.Multer.File,
+  cb: Function,
+) => {
+  if (!file) return cb(new Error('File is Empty'), false);
+
+  const fileExtension = file.mimetype.split('/')[1];
+  const validExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+
+  if (validExtensions.includes(fileExtension)) {
+    cb(null, true);
+  }
+
+  cb(null, false);
+};
+
+export const fileNamer = (
+  req: Express.Request,
+  file: Express.Multer.File,
+  cb: Function,
+) => {
+  if (!file) return cb(new Error('File is Empty'), false);
+
+  const fileExtension = file.mimetype.split('/')[1];
+
+  const fileName = `${uuid()}.${fileExtension}`;
+
+  cb(null, fileName);
+};
